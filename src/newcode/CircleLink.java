ca93d17282f44ccaa9b8855b8637427d3553abb9
co1de <562564173@qq.com>
2019-03-15 21:02:47 +0800

    找到链表中环的入口

--- src/newcode/CircleLink.java
+++ src/newcode/CircleLink.java
@@ -13,7 +13,9 @@ public class CircleLink {
     /** 
     * @Description: 思路很简单，找一个容器开始放入，如果容器中已有则说明进入了循环，返回当前节点，就是入口
      * 发现hashset更好，会插入不成功会返回false
-    * @Param:  
+     * 还有一种思路，获得长度，在遍历的时候每个位置+1，当有个位置值为2时，就是入口了。
+     *
+     * * @Param:
     * @return:  
     * @Author: co1de
     * @Date: 2019/3/15 
@@ -33,6 +35,30 @@ public class CircleLink {
             }
         }
         return null;
+    }
+
+    /**
+    * @Description: 再来一个思路：使用双指针，一个指针 fast 每次移动两个节点，一个指针 slow 每次移动一个节点。因为存在环，所以两个指针必定相遇在环中的某个节点上。假设相遇点在下图的 z1 位置，此时 fast 移动的节点数为 x+2y+z，slow 为 x+y，由于 fast 速度比 slow 快一倍，因此 x+2y+z=2(x+y)，得到 x=z。
+    在相遇点，slow 要到环的入口点还需要移动 z 个节点，如果让 fast 重新从头开始移动，并且速度变为每次移动一个节点，那么它到环入口点还需要移动 x 个节点。在上面已经推导出 x=z，因此 fast 和 slow 将在环入口点相遇。
+     * @Param:
+    * @return:
+    * @Author: co1de
+    * @Date: 2019/3/15
+    */
+//    public ListNode EntryNodeOfLoop(ListNode pHead) {
+//        if (pHead == null || pHead.next == null)
+//            return null;
+//        ListNode slow = pHead, fast = pHead;
+//        do {
+//            fast = fast.next.next;
+//            slow = slow.next;
+//        } while (slow != fast);
+//        fast = pHead;
+//        while (slow != fast) {
+//            slow = slow.next;
+//            fast = fast.next;
+//        }
+//        return slow;
     }
 
     public static void main(String[] args) {
