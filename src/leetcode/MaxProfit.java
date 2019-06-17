02d227d1bfe05b15e5fadf51cabd4d2e5351d724
co1de <562564173@qq.com>
2019-06-17 15:37:33 +0800

    股票最大收益

---
+++ src/leetcode/MaxProfit.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+/**
+ * @program: offer
+ * @description: Say you have an array for which the ith element is the price of a given stock on day i.  If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.  Note that you cannot sell a stock before you buy one.
+ * @author: co1de
+ * @create: 2019-06-17 15:21
+ **/
+
+public class MaxProfit {
+}
