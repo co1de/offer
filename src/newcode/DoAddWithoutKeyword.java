61b31fda47d5a1929c5230936b886785354ee6c2
co1de <562564173@qq.com>
2019-06-17 15:48:22 +0800

    不用关键字求和

---
+++ src/newcode/DoAddWithoutKeyword.java
@@ -0,0 +1,11 @@
+package newcode;
+
+/**
+ * @program: offer
+ * @description: 求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ * @author: co1de
+ * @create: 2019-06-17 15:38
+ **/
+
+public class DoAddWithoutKeyword {
+}
