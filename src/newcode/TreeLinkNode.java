8d209f7a522202e9b1d0af02e51a08f5ff7f2928
co1de <562564173@qq.com>
2019-06-18 17:55:31 +0800

    find binary tree next node in inorder

---
+++ src/newcode/TreeLinkNode.java
@@ -0,0 +1,19 @@
+package newcode;
+
+/**
+ * @program: offer
+ * @description:
+ * @author: co1de
+ * @create: 2019-06-18 17:04
+ **/
+
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
